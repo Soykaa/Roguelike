552c457fb6ee5ebfedb254e73924a9830a1e5d7f
lizazhemchuzhina <lizazhemchuzhina@mail.ru>
2022-04-10 13:52:56 +0300

    fix problem with tests: it is better to use virtual terminal instead of lanterna factory

--- src/test/java/ru/hse/roguelike/model/LevelTest.java
+++ src/test/java/ru/hse/roguelike/model/LevelTest.java
@@ -1,6 +1,7 @@
 package ru.hse.roguelike.model;
 
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.hse.roguelike.model.Characters.*;
@@ -18,7 +19,7 @@ public class LevelTest {
         board[1][0] = new Points(5);
         board[0][1] = new Inventory(InventoryItem.DESTROY);
         board[1][1] = new Obstacle();
-        var level = new Level(board, new GameScreenViewConsole(new DefaultTerminalFactory().createTerminal()), player, new HashMap<>(), CharacterType.SHELTER_LAVENDER, 5);
+        var level = new Level(board, new GameScreenViewConsole(new DefaultVirtualTerminal()), player, new HashMap<>(), CharacterType.SHELTER_LAVENDER, 5);
         level.moveCharacters(1, 0);
         Assertions.assertEquals(5, player.getPoints());
         Assertions.assertEquals(Result.IS_RUNNING, level.destroyObstacle());
