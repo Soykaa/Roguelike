5762c157d6e261214914588023c6eaa63aefcc9a
lizazhemchuzhina <lizazhemchuzhina@mail.ru>
2022-04-10 13:50:36 +0300

    check virtual terminal

--- src/test/java/ru/hse/roguelike/model/GameTest.java
+++ src/test/java/ru/hse/roguelike/model/GameTest.java
@@ -2,6 +2,7 @@ package ru.hse.roguelike.model;
 
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,8 @@ public class GameTest {
     @Test
     public void simpleGameTest() throws IOException {
         var game = new Game("");
-        game.startGame(false, new ConsoleViewFactory(new DefaultTerminalFactory().createTerminal()));
+        //new DefaultVirtualTerminal()
+        game.startGame(false, new ConsoleViewFactory( new DefaultVirtualTerminal()));
         Result result = game.makeAction(Action.CHANGE_EQUIPTION);
         Assertions.assertEquals(result.toString(), "IS_RUNNING");
     }
