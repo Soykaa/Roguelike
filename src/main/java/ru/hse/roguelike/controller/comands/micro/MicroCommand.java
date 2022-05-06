489ba7090242f741e17e45b6c3f08d5e6c285645
JaneKirillova <grindevald666@gmail.com>
2022-05-06 19:33:42 +0300

    Fix

--- src/main/java/ru/hse/roguelike/controller/comands/micro/MicroCommand.java
+++ src/main/java/ru/hse/roguelike/controller/comands/micro/MicroCommand.java
@@ -3,5 +3,5 @@ package ru.hse.roguelike.controller.comands.micro;
 import ru.hse.roguelike.controller.Screen;
 
 public interface MicroCommand {
-    public abstract Screen execute();
+    Screen execute();
 }
